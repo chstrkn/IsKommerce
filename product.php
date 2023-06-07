0dcf0e670c08ea32312674bc192146aa791c5d51
Chester Ken Gallego <chesterkengallego@gmail.com>
2023-06-07 22:44:26 +0800

    Update product.php

--- product.php
+++ product.php
@@ -1,5 +1,25 @@
 <?php
 session_start();
+$conn = mysqli_connect("localhost", "root", "", "iskommerce");
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+if (!isset($_SESSION['username'])) {
+  header("Location: register.php");
+}
+$product_id = $_GET['product_id'];
+$sql = "SELECT * FROM product WHERE product_id = $product_id";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+if (isset($_POST['add-to-cart'])) {
+  $quantity = $_POST['quantity'];
+  $sql = "SELECT user_id FROM user WHERE username = '" . $_SESSION['username'] . "'";
+  $user_id = mysqli_query($conn, $sql);
+  $user_id = mysqli_fetch_assoc($user_id)['user_id'];
+  $sql = "INSERT INTO cart_item (user_id, product_id, quantity, total) VALUES ($user_id, $product_id, $quantity, $quantity * " . $row['price'] . ")";
+  mysqli_query($conn, $sql);
+  echo "<script>alert('Added to cart!')</script>";
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,70 +33,48 @@ session_start();
   <link rel="shortcut icon" href="images/LOGO.png" type="image/x-icon" />
   <link rel="stylesheet" href="style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <script>
+    function changeImage(n) {
+      document.getElementById("main-img").src = document.getElementsByClassName("product-thumbnail")[n].src;
+    }
+  </script>
 </head>
 
-<body>
+<body id="demo">
   <header>
     <img id="logo" src="images/LOGO.png" alt="IsKommerce" />
-    <h1><a href="index.html">IsKommerce</a></h1>
-    <form id="search-bar" action="products.html" method="get">
+    <h1><a href="index.php">IsKommerce</a></h1>
+    <form id="search-bar" action="products.php" method="get">
       <input type="text" name="search" placeholder="Search" />
       <input type="submit" value="Search" />
     </form>
     <nav>
-      <a href="account.html">
+      <a href="account.php">
         <img src="images/account_circle_white_48dp.svg" alt="Account" />
       </a>
-      <a href="cart.html">
+      <a href="cart.php">
         <img src="images/shopping_bag_white_48dp.svg" alt="Cart" />
       </a>
     </nav>
   </header>
   <div id="product">
     <div id="product-image">
-      <img id="main-img" src="images/UPBEAT-University of the Philippines-UP Iloilo Shirt 2016.jpg" alt="" />
+      <img id="main-img" src="<?php echo $row['image']; ?>" alt="" />
     </div>
     <div id="product-info">
-      <div id="product-name">UPBEAT-University of the Philippines-UP Iloilo Shirt 2016</div>
+      <div id="product-name"><?php echo $row['name']; ?></div>
       <div id="product-brand">UPBEAT</div>
-      <div id="product-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, autem.</div>
-      <div id="product-price">₱250.00</div>
-      <div id="product-variant">
-        <label for="color">Color: </label>
-        <input type="radio" name="color" id="maroon" />
-        <label for="maroon">Maroon</label>
-        <input type="radio" name="color" id="blue" />
-        <label for="blue">Blue</label>
-        <input type="radio" name="color" id="yellow" />
-        <label for="yellow">Yellow</label>
-        <input type="radio" name="color" id="biege" />
-        <label for="biege">Biege</label>
-        <input type="radio" name="color" id="olive" required />
-        <label for="olive">Olive</label>
-      </div>
-      <div class="product-size">
-        <label for="size">Size: </label>
-        <input type="radio" name="size" id="small" />
-        <label for="small">Small</label>
-        <input type="radio" name="size" id="medium" />
-        <label for="medium">Medium</label>
-        <input type="radio" name="size" id="large" />
-        <label for="large">Large</label>
-      </div>
-      <div class="product-quantity">
+      <div id="product-description"><?php echo $row['description']; ?></div>
+      <div id="product-price">₱<?php echo $row['price']; ?></div>
+      <form class="product-quantity" method="post">
         <label for="quantity">Quantity:</label>
-        <input type="number" name="quantity" id="quantity" min="1" max="99" required />
-      </div>
-      <input type="button" value="Add to Cart" id="add-to-cart" />
-    </div>
-    <div id="product-thumbnails">
-      <img class="product-thumbnail" src="images/UPBEAT-University of the Philippines-UP Iloilo Shirt 2016.jpg" alt="" onclick="changeImage(0)" />
-      <img class="product-thumbnail" src="images/UPBEAT-University of the Philippines-UP Open University Shirt.jpg" alt="" onclick="changeImage(1)" />
-      <img class="product-thumbnail" src="images/UPBEAT-University of the Philippines-UP shirt 2021 BLACK.jpg" alt="" onclick="changeImage(2)" />
-      <img class="product-thumbnail" src="images/UPBEAT-University of the Philippines-UP Visayas Shirt 2017.jpg" alt="" onclick="changeImage(3)" />
+        <input type="number" name="quantity" id="quantity" min="1" max="99" value="1" required />
+        <br />
+        <input type="submit" name="add-to-cart" value="Add to Cart" />
+      </form>
     </div>
   </div>
-  <footer>Made with love. IsKommerce © 2023.</footer>
+  <footer>Made with love and PHP. IsKommerce © 2023.</footer>
   <script src="script.js"></script>
 </body>
 
